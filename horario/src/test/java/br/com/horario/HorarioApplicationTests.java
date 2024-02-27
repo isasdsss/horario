abd3d7233f393637ab1692efbecd8aa278893f92
Aluno <Aluno@ETEJK-LAB03-M08>
2024-02-26 21:05:27 -0300

    criação do projeto horario

---
+++ horario/src/test/java/br/com/horario/HorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
